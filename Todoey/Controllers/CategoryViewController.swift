c9ba74b89bad99e0e4c275052863ff5b39a73b14
Owner <Owner@Jesses-MacBook-Pro.local>
2019-01-17 20:04:15 +0800

    Model of Category, Category storyboard finished.  Lecture 244 finished.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Owner on 17/1/19.
+//  Copyright © 2019 Jesse-Team. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
